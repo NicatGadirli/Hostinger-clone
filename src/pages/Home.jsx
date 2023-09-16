6dabd5f17f15c43f216790e54c0bc06bce994811
NicatGadirli <nicatqedirli65@gmail.com>
2023-09-16 22:56:37 +0400

    Hostinger Clone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 //Sections
 import Hero from "./sections/Hero"
 import Evaluations from "./sections/Evaluations"
+import Hosting from "./sections/Hosting"
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <Evaluations />
+      <Hosting />
     </main>
   )
 }
--- src/pages/sections/Hosting.jsx
+++ src/pages/sections/Hosting.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+//Icons
+import { FaCheck } from 'react-icons/fa';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+
 
 const Hosting = () => {
-  return (
-    <div>Hosting</div>
-  )
+    return (
+        <section className="hosting">
+            <div className="container">
+                <div className="row">
+                    <div className="firstCard">
+                        <ul className="cardList">
+                            <li className="listItem">
+                                <div>
+                                    <FaCheck className="check" />
+                                    <strong>Free</strong>
+                                    Domain ($9.99 value)
+                                </div>
+                            </li>
+                            <li className="listItem">
+                                <div>
+                                    <FaCheck className="check" />
+                                    <strong>Free</strong>
+                                    Email
+                                </div>
+                                <div>
+                                    <AiOutlineInfoCircle className='info' />
+                                </div>
+                            </li>
+                            <li className="listItem">
+                                <div>
+                                    <FaCheck className="check" />
+                                    <strong>eCommerce</strong>
+                                    Features
+                                </div>
+                                <div>
+                                    <AiOutlineInfoCircle className='info' />
+                                </div>
+                            </li>
+                            <li className="listItem">
+                                <div>
+                                    <FaCheck className="check" />
+                                    <strong>Marketing</strong>
+                                    Integrations
+                                </div>
+                                <div>
+                                    <AiOutlineInfoCircle className='info' />
+                                </div>
+                            </li>
+                            <li className="listItem">
+                                <div>
+                                    <FaCheck className="check" />
+                                    <strong>24/7</strong>
+                                    Customer Support
+                                </div>
+                                <div>
+                                    <AiOutlineInfoCircle className='info' />
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="secondCard">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Hosting
